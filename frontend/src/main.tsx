aa517d6c31730fde29b61244f83c6db0911d1fed
pornchanok <pornchanok.phua@mail.kmutt.ac.th>
2025-05-19 10:16:32 +0700

    set up frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"; 
+import Register from './pages/Register.tsx';
+import Home from './pages/Home.tsx';
+import LogIn from './pages/LogIn.tsx';
+import Detail from './pages/Detail.tsx';
+import AddNewSong from './pages/AddNewSong.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <Home />, 
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+  {
+    path: "/login",
+    element: <LogIn />,
+  },
+  {
+    path: "/detail",
+    element: <Detail />,
+  },
+  {
+    path: "/addnewsong",
+    element: <AddNewSong />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
+
---
+++ frontend/src/pages/AddNewSong.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddNewSong() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddNewSong
