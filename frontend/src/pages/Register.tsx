74d07a5713f417057a437c51a07686c9e2ac0723
pornchanok <pornchanok.phua@mail.kmutt.ac.th>
2025-05-20 15:55:04 +0700

    home

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -102,7 +102,7 @@ const Register = () => {
     "Please correct the errors in the form";
 
   return (
-    <div className="w-full h-screen bg-[#262626] overflow-hidden relative flex justify-center items-center">
+    <div className="w-full h-screen bg-[#262626] overflow-hidden relative flex justify-center items-center font-[libre-caslon-text]">
       <div className="z-10 flex flex-col items-center w-full max-w-md px-4">
         {registerStatus && (
           <div
